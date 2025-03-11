77436b13d00aac81faf05b6cc931aa02ea9722eb
Yohani <7882441+nany-5790@users.noreply.github.com>
2025-03-11 09:28:42 -0400

    Update

--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+"""Django's command-line utility for administrative tasks. Project created by Yohani Rodriguez"""
 import os
 import sys
 
