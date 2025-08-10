63c191ef9543bfbb8a18b9866689e049c5ea7c25
omar <padrinogtr@gmail.com>
2025-08-10 16:11:21 -0400

    Update views.py
    Add driver_input view

--- lmsapp/views.py
+++ lmsapp/views.py
@@ -889,3 +889,10 @@ def report_loads_per_customer(request):
         'q': q,
     }
     return render(request, 'lmsapp/report_loads_per_customer.html', data)
+from django.shortcuts import render
+from django.contrib import messages
+
+def driver_input(request):
+    if request.method == 'POST':
+        messages.success(request, "Form submitted successfully!")
+    return render(request, 'driver_input.html')
